1835c2f42058e58d9d5293b7fa75f45b8156eb0b
Sammi Song <songyunhui2008@gmail.com>
2018-04-29 22:32:44 -0700

    Add spacing variables.

---
+++ src/Variables/Spacing.js
@@ -0,0 +1,19 @@
+
+export const spacer = '1rem !default'
+export const spacers = {
+    _0: 0,
+    _1: spacer * .25,
+    _2: spacer * .5,
+    _3: spacer,
+    _4: spacer * 1.5,
+    _5: spacer * 3
+}
+
+// This variable affects the `.h-*` and `.w-*` classes.
+export const sizes = {
+  _25: '25%',
+  _50: '50%',
+  _75: '75%',
+  _100: '100%',
+  auto: 'auto'
+}
---
+++ src/Variables/Spacing.test.js
@@ -0,0 +1,32 @@
+import {
+    spacer,
+    spacers,
+    sizes
+} from './Spacing'
+
+// Control the default styling of most Bootstrap elements by modifying these
+// variables. Mostly focused on spacing.
+// You can add more entries to the $spacers map, should you need more variation.
+describe('Spacing', () => {
+    it('Quickly modify global styling by enabling or disabling optional features.', () => {
+        
+        expect(spacer).toEqual('1rem !default')
+
+        expect(spacers).toEqual({
+            _0: 0,
+            _1: spacer * .25,
+            _2: spacer * .5,
+            _3: spacer,
+            _4: spacer * 1.5,
+            _5: spacer * 3
+        })
+
+        expect(sizes).toEqual({
+            _25: '25%',
+            _50: '50%',
+            _75: '75%',
+            _100: '100%',
+            auto: 'auto'
+        })
+    })
+})
