82df40cbf7a3999815bc147650e4f86f174a7572
Yunhui <songyunhui2008@gmail.com>
2018-04-30 20:04:33 -0700

    Add more tests for darken function.

--- src/Functions/darken.test.js
+++ src/Functions/darken.test.js
@@ -3,5 +3,6 @@ import {darken} from './darken'
 describe('darken', () => {
     it('darken', () => {
         expect(darken('#FFF', '0%')).toEqual('#FFFFFF')
+        expect(darken('#AAbBCD', '10%')).toEqual('#92A8BF')
     })
 })
