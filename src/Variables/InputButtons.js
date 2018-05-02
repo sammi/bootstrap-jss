08b11553603b45f38abc9b45955303b22928871a
Sammi Song <songyunhui2008@gmail.com>
2018-05-01 20:27:18 -0700

    Add input button shared variables.

---
+++ src/Variables/InputButtons.js
@@ -0,0 +1,22 @@
+import {lineHeightBase, lineHeightSm, lineHeightLg} from './Fonts'
+import format from 'string-format'
+import {rgba} from '../Functions/rgba'
+import {componentActiveBg, borderWidth} from './Components'
+
+export const inputBtnPaddingY = '.375rem !default'
+export const inputBtnPaddingX = '.75rem !default'
+export const inputBtnLineHeight = lineHeightBase
+
+export const inputBtnFocusWidth = '.2rem !default'
+export const inputBtnFocusColor = format('{} !default', rgba(componentActiveBg, .25))
+export const inputBtnFocusBoxShadow = format('0 0 0 {} {} !default', inputBtnFocusWidth, inputBtnFocusColor)
+
+export const inputBtnPaddingYSm = '.25rem !default'
+export const inputBtnPaddingXSm = '.5rem !default'
+export const inputBtnLineHeightSm = lineHeightSm
+
+export const inputBtnPaddingYLg = '.5rem !default'
+export const inputBtnPaddingXLg = '1rem !default'
+export const inputBtnLineHeightLg = lineHeightLg
+
+export const inputBtnBorderWidth = borderWidth
---
+++ src/Variables/InputButtons.test.js
@@ -0,0 +1,42 @@
+import {lineHeightBase, lineHeightSm, lineHeightLg} from './Fonts'
+import format from 'string-format'
+import {rgba} from '../Functions/rgba'
+import {componentActiveBg, borderWidth} from './Components'
+
+import {
+    inputBtnPaddingY,
+    inputBtnPaddingX,
+    inputBtnLineHeight,
+    inputBtnFocusWidth,
+    inputBtnFocusColor,
+    inputBtnFocusBoxShadow,
+    inputBtnPaddingYSm,
+    inputBtnPaddingXSm,
+    inputBtnLineHeightSm,
+    inputBtnPaddingYLg,
+    inputBtnPaddingXLg,
+    inputBtnLineHeightLg,
+    inputBtnBorderWidth
+} from './InputButtons'
+
+describe('Buttons + Forms', () => {
+    it('Shared variables that are reassigned to `input-` and `btn-` specific variables.', () => {
+        expect(inputBtnPaddingY).toEqual('.375rem !default')
+        expect(inputBtnPaddingX).toEqual('.75rem !default')
+        expect(inputBtnLineHeight).toEqual(lineHeightBase)
+        
+        expect(inputBtnFocusWidth).toEqual('.2rem !default')
+        expect(inputBtnFocusColor).toEqual(format('{} !default', rgba(componentActiveBg, .25)))
+        expect(inputBtnFocusBoxShadow).toEqual(format('0 0 0 {} {} !default', inputBtnFocusWidth, inputBtnFocusColor))
+        
+        expect(inputBtnPaddingYSm).toEqual('.25rem !default')
+        expect(inputBtnPaddingXSm).toEqual('.5rem !default')
+        expect(inputBtnLineHeightSm).toEqual(lineHeightSm)
+        
+        expect(inputBtnPaddingYLg).toEqual('.5rem !default')
+        expect(inputBtnPaddingXLg).toEqual('1rem !default')
+        expect(inputBtnLineHeightLg).toEqual(lineHeightLg)
+        
+        expect(inputBtnBorderWidth).toEqual(borderWidth)
+    })
+})
