4cb70132ed7fdf2cf3f1fe8154a785487e092894
Yunhui <songyunhui2008@gmail.com>
2018-05-01 19:34:57 -0700

    Add lighten function.

---
+++ src/Functions/lighten.js
@@ -0,0 +1,15 @@
+import Color from 'color'
+import _ from 'lodash'
+import percentValue from 'percent-value'
+
+export const lighten = (rawColor, percent) => {
+
+    const rgbColor = _.replace(rawColor, '!default', '').trim()
+
+    const ratio = percentValue(percent).from(1)
+
+    const lighternColor = Color(rgbColor).lighten(ratio)
+
+    return lighternColor.hex()
+    
+}
---
+++ src/Functions/lighten.test.js
@@ -0,0 +1,8 @@
+import {lighten} from './lighten'
+
+describe('lighten', () => {
+    it('lighten', () => {
+        expect(lighten('#FFF', '0%')).toEqual('#FFFFFF')
+        expect(lighten('#AAbBCD', '10%')).toEqual('#C2CEDB')
+    })
+})
