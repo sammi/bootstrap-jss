7f11fffe4773e8d9f515eaee074ebb6cfcf6e24b
Sammi Song <songyunhui2008@gmail.com>
2018-05-03 22:30:21 -0700

    Add Close variables.

---
+++ src/Variables/Close.js
@@ -0,0 +1,13 @@
+import format from 'string-format'
+import {fontWeightBold, fontSizeBase} from './Fonts'
+import {black, white} from './Colors'
+import {size} from '../Functions/size'
+
+export const closeFontSize = format(
+    '{}{} !default',
+    size(fontSizeBase).value * 1.5,
+    size(fontSizeBase).unit
+)
+export const closeFontWeight = fontWeightBold
+export const closeColor = black
+export const closeTextShadow = format('0 1px 0 {} !default', white)
---
+++ src/Variables/Close.test.js
@@ -0,0 +1,24 @@
+import format from 'string-format'
+import {fontWeightBold, fontSizeBase} from './Fonts'
+import {black, white} from './Colors'
+import {size} from '../Functions/size'
+
+import {
+    closeFontSize,
+    closeFontWeight,
+    closeColor,
+    closeTextShadow
+} from './Close'
+
+describe('Close', () => {
+    it('Close', () => {
+        expect(closeFontSize).toEqual(format(
+            '{}{} !default',
+            size(fontSizeBase).value * 1.5,
+            size(fontSizeBase).unit
+        ))
+        expect(closeFontWeight).toEqual(fontWeightBold)
+        expect(closeColor).toEqual(black)
+        expect(closeTextShadow).toEqual(format('0 1px 0 {} !default', white))
+    })
+})
