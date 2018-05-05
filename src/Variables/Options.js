094399251bf00a41afb898fb311a79b63128ea16
Sammi Song <songyunhui2008@gmail.com>
2018-05-04 21:41:48 -0700

    replace boolean string by type.

--- src/Variables/Options.js
+++ src/Variables/Options.js
@@ -1,8 +1,8 @@
-export const enableCaret = 'true'
-export const enableRounded = 'true'
-export const enableShadows = 'false'
-export const enableGradients = 'false'
-export const enableTransitions = 'true'
-export const enableHoverMediaQuery = 'false'
-export const enableGridClasses = 'true'
-export const enablePrintStyles = 'true'
+export const enableCaret = true
+export const enableRounded = true
+export const enableShadows = false
+export const enableGradients = false
+export const enableTransitions = true
+export const enableHoverMediaQuery = false
+export const enableGridClasses = true
+export const enablePrintStyles = true
--- src/Variables/Options.test.js
+++ src/Variables/Options.test.js
@@ -11,15 +11,15 @@ import {
 
 describe('Options', () => {
     it('Quickly modify global styling by enabling or disabling optional features.', () => {
-        expect(enableCaret).toEqual('true')
-        expect(enableRounded).toEqual('true')
-        expect(enableShadows).toEqual('false')
-        expect(enableGradients).toEqual('false')
-        expect(enableTransitions).toEqual('true')
+        expect(enableCaret).toEqual(true)
+        expect(enableRounded).toEqual(true)
+        expect(enableShadows).toEqual(false)
+        expect(enableGradients).toEqual(false)
+        expect(enableTransitions).toEqual(true)
         // Deprecated, no longer affects any compiled CSS
-        expect(enableHoverMediaQuery).toEqual('false')
-        expect(enableGridClasses).toEqual('true')
-        expect(enablePrintStyles).toEqual('true')
+        expect(enableHoverMediaQuery).toEqual(false)
+        expect(enableGridClasses).toEqual(true)
+        expect(enablePrintStyles).toEqual(true)
     })
 
 })
