8f9de941245ccb95ccac0a2a19637873be4130ae
Sammi Song <songyunhui2008@gmail.com>
2018-05-05 22:06:20 -0700

    Add backgroundVariant functions.

--- src/Mixin/backgroundVariant.js
+++ src/Mixin/backgroundVariant.js
@@ -1,8 +1,27 @@
 import {darken} from '../Functions/darken'
 import {mix} from '../Functions/mix'
 import format from 'string-format'
+import {hoverFocus} from './hover'
+import {bodyBg} from '../Variables/Body'
 
-export const bgVariant = (parent, color) => ({
-})
+export const bgVariant = (parent, color) => {
+    let style = {}
+    style[parent] = {backgroundColor: format('{} !important', color)}
+    style[format('a.{}, button.{}', parent, parent)] = {
+        extend: hoverFocus({
+            backgroundColor: format('{} !important', darken(color, '10%'))
+        })
+    }
+    return style
+}
 
-export const bgGradientVariant = (parent, color) => ({})
+export const bgGradientVariant = (parent, color) => {
+    let style = {}
+    style[parent] = {backgroundColor: format(
+        '{} {} {} repeat-x !important', 
+        color, 
+        format('linear-gradient(180deg, {}', mix(bodyBg, color, '15%')), 
+        color
+    )}
+    return style
+}
--- src/Mixin/backgroundVariant.test.js
+++ src/Mixin/backgroundVariant.test.js
@@ -1,8 +1,36 @@
 import {bgVariant, bgGradientVariant} from './backgroundVariant'
+
+import {darken} from '../Functions/darken'
+
+import {hoverFocus} from './hover'
+import format from 'string-format'
+import {bodyBg} from '../Variables/Body'
+import {mix} from '../Functions/mix'
+
 describe('Contextual backgrounds', () => {
-    it('Contextual backgrounds', () => {
-        const parent = 'parentClass'
-        expect(bgVariant(parent, '#fff')).toEqual({})
-        expect(bgGradientVariant(parent, '#abc')).toEqual({})
+    const parent = 'parentClass'
+    const color = '#abc'
+
+    it('bgVariant', () => {
+        expect(bgVariant(parent, color)).toEqual({
+            'parentClass': {backgroundColor: format('{} !important', color)},
+            'a.parentClass, button.parentClass': {
+                extend: hoverFocus({
+                    backgroundColor: format('{} !important', darken(color, '10%'))
+                })
+            }
+        })
     })
+
+    it('bgGradientVariant', () => {
+        expect(bgGradientVariant(parent, color)).toEqual({
+            'parentClass': {backgroundColor: format(
+                '{} {} {} repeat-x !important', 
+                color, 
+                format('linear-gradient(180deg, {}', mix(bodyBg, color, '15%')), 
+                color
+            )}
+        })
+    })
+    
 })
