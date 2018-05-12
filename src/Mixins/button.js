ab72c0c3b0f38f87a28bfa770fb6f2773ff1146e
Sammi Song <songyunhui2008@gmail.com>
2018-05-12 15:31:08 -0700

    remove extra return word.

--- src/Mixins/button.js
+++ src/Mixins/button.js
@@ -20,44 +20,42 @@ export const buttonVariant = (
   myBtnBoxShadow = btnBoxShadow,
   myBtnFocusWidth = btnFocusWidth,
   myBtnActiveBoxShadow = btnActiveBoxShadow
-) => {
-  return {
+) => ({
+  color: colorYiq(backgroundColor),
+  ...gradientBg(backgroundColor),
+  borderColor: borderColor,
+  ...boxShadow(myBtnBoxShadow),
+  ...hover({
+    color: colorYiq(hoverBackgroundColor),
+    ...gradientBg(hoverBackgroundColor),
+    borderColor: hoverBorderColor
+  }),
+  '&:focus, &.focus': {
+    boxShadow: enableShadows
+      ? format('{}, 0 0 0 {} {}', myBtnBoxShadow, myBtnFocusWidth, rgba(borderColor, 0.5))
+      : format('0 0 0 {} {}', myBtnFocusWidth, rgba(borderColor, 0.5))
+  },
+  '&.disabled, &:disabled': {
     color: colorYiq(backgroundColor),
-    ...gradientBg(backgroundColor),
-    borderColor: borderColor,
-    ...boxShadow(myBtnBoxShadow),
-    ...hover({
-      color: colorYiq(hoverBackgroundColor),
-      ...gradientBg(hoverBackgroundColor),
-      borderColor: hoverBorderColor
-    }),
-    '&:focus, &.focus': {
-      boxShadow: enableShadows
-        ? format('{}, 0 0 0 {} {}', myBtnBoxShadow, myBtnFocusWidth, rgba(borderColor, 0.5))
-        : format('0 0 0 {} {}', myBtnFocusWidth, rgba(borderColor, 0.5))
-    },
-    '&.disabled, &:disabled': {
-      color: colorYiq(backgroundColor),
-      backgroundColor: backgroundColor,
-      borderColor: borderColor
-    },
-    '&:not(:disabled):not(.disabled):active, &:not(:disabled):not(.disabled).active, .show > &.dropdown-toggle':
-    enableGradients
-      ? {
-        color: colorYiq(activeBackgroundColor),
-        backgroundColor: activeBackgroundColor,
-        backgrounImage: 'none',
-        borderColor: activeBorderColor
-      }
-      : {
-        color: colorYiq(activeBackgroundColor),
-        backgroundColor: activeBackgroundColor,
-        borderColor: activeBorderColor
-      },
-    '&:focus': {
-      boxShadow: enableShadows
-        ? format('{}, 0 0 0 {} {}', myBtnActiveBoxShadow, myBtnFocusWidth, rgba(borderColor, 0.5))
-        : format('0 0 0 {} {}', myBtnFocusWidth, rgba(borderColor, 0.5))
+    backgroundColor: backgroundColor,
+    borderColor: borderColor
+  },
+  '&:not(:disabled):not(.disabled):active, &:not(:disabled):not(.disabled).active, .show > &.dropdown-toggle':
+  enableGradients
+    ? {
+      color: colorYiq(activeBackgroundColor),
+      backgroundColor: activeBackgroundColor,
+      backgrounImage: 'none',
+      borderColor: activeBorderColor
     }
+    : {
+      color: colorYiq(activeBackgroundColor),
+      backgroundColor: activeBackgroundColor,
+      borderColor: activeBorderColor
+    },
+  '&:focus': {
+    boxShadow: enableShadows
+      ? format('{}, 0 0 0 {} {}', myBtnActiveBoxShadow, myBtnFocusWidth, rgba(borderColor, 0.5))
+      : format('0 0 0 {} {}', myBtnActiveBoxShadow, myBtnFocusWidth, rgba(borderColor, 0.5))
   }
-}
+})
