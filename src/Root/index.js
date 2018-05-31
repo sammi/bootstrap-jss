7dfffb6038267e2de8dacf5d0068d758a1c723c9
Sammi Song <songyunhui2008@gmail.com>
2018-05-30 22:57:35 -0700

    Add missing css root variables.

--- src/Root/index.js
+++ src/Root/index.js
@@ -1,10 +1,13 @@
 import { colors, themeColors } from '../Variables/Colors'
 import { gridBreakpoints } from '../Variables/Grid'
+import { fontFamilySansSerif, fontFamilyMonospace } from '../Variables/Fonts'
 
 let rootVariables = {}
 Object.keys(colors).forEach(color => { rootVariables[`--${color}`] = colors[color] })
 Object.keys(themeColors).forEach(themeColor => { rootVariables[`--${themeColor}`] = themeColors[themeColor] })
 Object.keys(gridBreakpoints).forEach(gridBreakpoint => { rootVariables[`--breakpoing-${gridBreakpoint}`] = gridBreakpoints[gridBreakpoint] })
+rootVariables['--font-family-sans-serif'] = fontFamilySansSerif
+rootVariables['--font-family-monospace'] = fontFamilyMonospace
 
 export default {
   ':root': rootVariables
--- src/Root/index.test.js
+++ src/Root/index.test.js
@@ -1,5 +1,6 @@
 import { colors, themeColors } from '../Variables/Colors'
 import { gridBreakpoints } from '../Variables/Grid'
+import { fontFamilySansSerif, fontFamilyMonospace } from '../Variables/Fonts'
 
 import root from './index'
 
@@ -9,6 +10,9 @@ describe('root', () => {
     Object.keys(colors).forEach(color => { rootVariables[`--${color}`] = colors[color] })
     Object.keys(themeColors).forEach(themeColor => { rootVariables[`--${themeColor}`] = themeColors[themeColor] })
     Object.keys(gridBreakpoints).forEach(gridBreakpoint => { rootVariables[`--breakpoing-${gridBreakpoint}`] = gridBreakpoints[gridBreakpoint] })
+
+    rootVariables['--font-family-sans-serif'] = fontFamilySansSerif
+    rootVariables['--font-family-monospace'] = fontFamilyMonospace
 
     expect(root).toEqual({
       ':root': {
