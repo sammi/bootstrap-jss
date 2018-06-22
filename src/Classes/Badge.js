f1c2c2a6f81d01d7b07da9db45f9f95ce1ecd10f
Yunhui <songyunhui2008@gmail.com>
2018-06-21 19:55:34 -0700

    Add Badge class WIP.

--- src/Classes/Badge.js
+++ src/Classes/Badge.js
@@ -1,17 +1,55 @@
-import {badgePaddingY, badgePaddingX, badgeFontSize, badgeFontWeight, badgeBorderRadius} from '../Variables/Badges'
-import { borderRadius } from '../Mixins/borderRadius'
+import {
+  badgePaddingY,
+  badgePaddingX,
+  badgeFontSize,
+  badgeFontWeight,
+  badgeBorderRadius,
+  badgePillPaddingX,
+  badgePillBorderRadius
+} from '../Variables/Badges'
+
+import {
+  borderRadius
+} from '../Mixins/borderRadius'
+
+import {
+  themeColors
+} from '../Variables/Colors'
+import _ from 'lodash'
+import {
+  badgeVariant
+} from '../Mixins/badge'
 
 export const badge = {
-    display: 'inline-block',
-    padding: `${badgePaddingY} ${badgePaddingX}`,
-    fontSize: badgeFontSize,
-    fontWeight: badgeFontWeight,
-    lineHeight: 1,
-    textAlign: 'center',
-    whiteSpace: 'nowrap',
-    verticalAlign: 'baseline',
-    ...borderRadius(badgeBorderRadius),
-    '&:empty': {
-      display: 'none'
-    }
+  display: 'inline-block',
+  padding: `${badgePaddingY} ${badgePaddingX}`,
+  fontSize: badgeFontSize,
+  fontWeight: badgeFontWeight,
+  lineHeight: 1,
+  textAlign: 'center',
+  whiteSpace: 'nowrap',
+  verticalAlign: 'baseline',
+  ...borderRadius(badgeBorderRadius),
+  '&:empty': {
+    display: 'none'
+  }
+}
+
+export const badgeInButton = {
+  position: 'relative',
+  top: '-1px'
+}
+
+export const badgePill = {
+  paddingRight: badgePillPaddingX,
+  paddingLeft: badgePillPaddingX,
+  ...borderRadius(badgePillBorderRadius)
+}
+
+export const badgeThemeColors = () => {
+  let badgeColors = {}
+  Object.keys(themeColors).forEach(themeColorName => {
+    badgeColors['badge' + _.upperFirst(themeColorName)] = badgeVariant(themeColors[themeColorName])
+  })
+  return badgeColors
 }
--- src/Classes/Badge.test.js
+++ src/Classes/Badge.test.js
@@ -1,25 +1,66 @@
+import {
+  badgePaddingY,
+  badgePaddingX,
+  badgeFontSize,
+  badgeFontWeight,
+  badgeBorderRadius,
+  badgePillPaddingX,
+  badgePillBorderRadius
+} from '../Variables/Badges'
 
-import {badgePaddingY, badgePaddingX, badgeFontSize, badgeFontWeight, badgeBorderRadius} from '../Variables/Badges'
-import { borderRadius } from '../Mixins/borderRadius'
+import {
+  borderRadius
+} from '../Mixins/borderRadius'
 
-import {badge} from './Badge'
+import {
+  badge,
+  badgeInButton,
+  badgePill
+} from './Badge'
 
 describe('Badge classes', () => {
 
-    it('badge', () => {
-        expect(badge).toEqual({
-            display: 'inline-block',
-            padding: `${badgePaddingY} ${badgePaddingX}`,
-            fontSize: badgeFontSize,
-            fontWeight: badgeFontWeight,
-            lineHeight: 1,
-            textAlign: 'center',
-            whiteSpace: 'nowrap',
-            verticalAlign: 'baseline',
-            ...borderRadius(badgeBorderRadius),
-            '&:empty': {
-              display: 'none'
-            }
-        })
+  it('badge', () => {
+    expect(badge).toEqual({
+      display: 'inline-block',
+      padding: `${badgePaddingY} ${badgePaddingX}`,
+      fontSize: badgeFontSize,
+      fontWeight: badgeFontWeight,
+      lineHeight: 1,
+      textAlign: 'center',
+      whiteSpace: 'nowrap',
+      verticalAlign: 'baseline',
+      ...borderRadius(badgeBorderRadius),
+      '&:empty': {
+        display: 'none'
+      }
     })
+  })
+
+  it('quick fix for badges in buttons', () => {
+    //.btn .badge
+    expect(badgeInButton).toEqual({
+      position: 'relative',
+      top: '-1px'
+    })
+  })
+
+  it('Pill badges, Make them extra rounded with a modifier to replace v3\'s badges.', () => {
+    expect(badgePill).toEqual({
+      paddingRight: badgePillPaddingX,
+      paddingLeft: badgePillPaddingX,
+      ...borderRadius(badgePillBorderRadius)
+    })
+  })
+
 })
+
+// Colors
+//
+// Contextual variations (linked badges get darker on :hover).
+
+//   @each $color, $value in $theme-colors {
+//     .badge-#{$color} {
+//       @include badge-variant($value);
+//     }
+//   }
