3a47b18ce7ac635fac20c0c4adbe55ef8016f6af
Yunhui <songyunhui2008@gmail.com>
2018-06-27 19:59:07 -0700

    Add empty badge story.

--- stories/.storybook/config.js
+++ stories/.storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from '@storybook/react'
 function loadStories() {
   require('../src/reboot.js')
   require('../src/alert.js')
+  require('../src/badge.js')
 }
 
 configure(loadStories, module)
---
+++ stories/src/badge.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+storiesOf('Badge', module)
+  .add('Badge', () =>
+    <div>
+      Badge
+    </div>
+  )
