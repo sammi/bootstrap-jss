92f80be45e414115a2a9e6be935ebc28e9d12f9e
Sammi Song <songyunhui2008@gmail.com>
2018-08-23 20:57:57 -0700

    Solve demo slow response preformance issue.  realod image + isolate plugin are evils.

--- stories/src/reboot.js
+++ stories/src/reboot.js
@@ -8,13 +8,6 @@ import { GroupingContent } from './reboot/GroupingContent'
 import { TabularData } from './reboot/TabularData'
 import { Forms } from './reboot/Forms'
 
-import injectSheet, {jss} from 'react-jss'
-
-import * as Bootstrap from 'bootstrap-jss'
-import isolate from 'jss-isolate'
-
-jss.use(isolate({ reset: {...Bootstrap.Reboot} }))
-
 storiesOf('Reboot', module)
   .add('Reboot elements', () => <div>
       <Heading />
