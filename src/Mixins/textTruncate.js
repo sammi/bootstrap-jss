5869c1a3f7a88b1061c63f248e5e1764a8a87525
Sammi Song <songyunhui2008@gmail.com>
2018-08-30 23:36:13 -0700

    Add text truncate mixin.

---
+++ src/Mixins/textTruncate.js
@@ -0,0 +1,5 @@
+export const textTruncate = {
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap'
+}
--- src/Mixins/textTruncate.test.js
+++ src/Mixins/textTruncate.test.js
@@ -1,5 +1,13 @@
-describe('', () => {
-  it('', () => {
+import { textTruncate } from './textTruncate'
 
+// Text truncate
+// Requires inline-block or block for proper styling
+describe('textTruncate', () => {
+  it('textTruncate', () => {
+    expect(textTruncate).toEqual({
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      whiteSpace: 'nowrap'
+    })
   })
 })
