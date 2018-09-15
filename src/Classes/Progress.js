97300ac188d0f30e444975fffc3aea682a864844
Sammi Song <songyunhui2008@gmail.com>
2018-09-15 14:26:37 -0700

    Add progess classes and missing gradient mixins.

---
+++ src/Classes/Progress.js
@@ -0,0 +1,58 @@
+import { progressHeight, progressFontSize, progressBg,
+  progressBborderRadius, progressBoxShadow, progressBarColor,
+  progressBarBg, progressBarTransition,
+  progressBarAnimationTiming
+} from '../Variables/Progressbars'
+import { borderRadius } from '../Mixins/borderRadius'
+import { boxShadow } from '../Mixins/boxShadow'
+import { transition } from '../Mixins/transition'
+import { gradientStriped } from '../Mixins/gradients'
+
+const progressBarStripes = {
+  '@keyframes progressBarStripes': {
+    from: {
+      backgroundPosition: `${progressHeight} 0`
+    },
+    to: {
+      backgroundPosition: '0 0'
+    }
+  }
+}
+
+const progress = {
+  display: 'flex',
+  height: progressHeight,
+  overflow: 'hidden',
+  fontSize: progressFontSize,
+  backgroundColor: progressBg,
+  ...borderRadius(progressBborderRadius),
+  ...boxShadow(progressBoxShadow)
+}
+
+const progressBar = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  color: progressBarColor,
+  textAlign: 'center',
+  whiteSpace: 'nowrap',
+  backgroundColor: progressBarBg,
+  ...transition(progressBarTransition)
+}
+
+const progressBarStiped = {
+  ...gradientStriped(),
+  backgroundSize: `${progressHeight} ${progressHeight}`
+}
+
+const progressBarAnimated = {
+  animation: `progressBarStripes ${progressBarAnimationTiming}`
+}
+
+export {
+  progressBarStripes,
+  progress,
+  progressBar,
+  progressBarStiped,
+  progressBarAnimated
+}
---
+++ src/Classes/Progress.test.js
@@ -0,0 +1,64 @@
+import { progressHeight, progressFontSize, progressBg,
+  progressBborderRadius, progressBoxShadow, progressBarColor,
+  progressBarBg, progressBarTransition,
+  progressBarAnimationTiming
+} from '../Variables/Progressbars'
+import { borderRadius } from '../Mixins/borderRadius'
+import { boxShadow } from '../Mixins/boxShadow'
+import { transition } from '../Mixins/transition'
+import { gradientStriped } from '../Mixins/gradients'
+
+import { progressBarStripes, progress, progressBar, progressBarStiped, progressBarAnimated } from './Progress'
+
+describe('progress', () => {
+  it('progress-bar-strips', () => {
+    expect(progressBarStripes).toEqual({
+      '@keyframes progressBarStripes': {
+        from: {
+          backgroundPosition: `${progressHeight} 0`
+        },
+        to: {
+          backgroundPosition: '0 0'
+        }
+      }
+    })
+  })
+
+  it('progress', () => {
+    expect(progress).toEqual({
+      display: 'flex',
+      height: progressHeight,
+      overflow: 'hidden',
+      fontSize: progressFontSize,
+      backgroundColor: progressBg,
+      ...borderRadius(progressBborderRadius),
+      ...boxShadow(progressBoxShadow)
+    })
+  })
+
+  it('progess-bar', () => {
+    expect(progressBar).toEqual({
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      color: progressBarColor,
+      textAlign: 'center',
+      whiteSpace: 'nowrap',
+      backgroundColor: progressBarBg,
+      ...transition(progressBarTransition)
+    })
+  })
+
+  it('progress-bar-striped', () => {
+    expect(progressBarStiped).toEqual({
+      ...gradientStriped(),
+      backgroundSize: `${progressHeight} ${progressHeight}`
+    })
+  })
+
+  it('progress-bar-animated', () => {
+    expect(progressBarAnimated).toEqual({
+      animation: `progressBarStripes ${progressBarAnimationTiming}`
+    })
+  })
+})
