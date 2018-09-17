d34c19afc39624cb14f0bba4a23a0829f6d90cba
Sammi Song <songyunhui2008@gmail.com>
2018-09-16 19:23:31 -0700

    Add Align utility class.

---
+++ src/Utilities/Align.js
@@ -0,0 +1,23 @@
+export const alignBaseline = {
+  verticalAlign: 'baseline !important'
+}
+
+export const alignTop = {
+  verticalAlign: 'top !important'
+}
+
+export const alignMiddle = {
+  verticalAlign: 'middle !important'
+}
+
+export const alignBottom = {
+  verticalAlign: 'bottom !important'
+}
+
+export const alignTextBottom = {
+  verticalAlign: 'text-bottom !important'
+}
+
+export const alignTextTop = {
+  verticalAlign: 'text-top !important'
+}
--- src/Utilities/Align.test.js
+++ src/Utilities/Align.test.js
@@ -1,5 +1,38 @@
+import { alignBaseline, alignTop, alignMiddle, alignBottom, alignTextBottom, alignTextTop } from './Align'
 describe('', () => {
-  it('', () => {
+  it('align-baseline', () => {
+    expect(alignBaseline).toEqual({
+      verticalAlign: 'baseline !important'
+    })
+  })
 
+  it('align-top', () => {
+    expect(alignTop).toEqual({
+      verticalAlign: 'top !important'
+    })
+  })
+
+  it('align-middle', () => {
+    expect(alignMiddle).toEqual({
+      verticalAlign: 'middle !important'
+    })
+  })
+
+  it('align-bottom', () => {
+    expect(alignBottom).toEqual({
+      verticalAlign: 'bottom !important'
+    })
+  })
+
+  it('align-text-bottom', () => {
+    expect(alignTextBottom).toEqual({
+      verticalAlign: 'text-bottom !important'
+    })
+  })
+
+  it('align-text-top', () => {
+    expect(alignTextTop).toEqual({
+      verticalAlign: 'text-top !important'
+    })
   })
 })
