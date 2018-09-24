42447494b954ceeb97814c9e4d3844bfb298fd2c
Yunhui <songyunhui2008@gmail.com>
2018-09-23 21:35:06 -0700

    Add screen reader utility class.

---
+++ src/Utilities/Screenreaders.js
@@ -0,0 +1,10 @@
+import { srOnly as seOnlyMixin, srOnlyFocusable as srOnlyFocusableMixin } from '../Mixins/screenReader'
+
+const srOnly = seOnlyMixin
+
+const srOnlyFocusable = srOnlyFocusableMixin
+
+export {
+  srOnly,
+  srOnlyFocusable
+}
--- src/Utilities/Screenreaders.test.js
+++ src/Utilities/Screenreaders.test.js
@@ -1,5 +1,13 @@
-describe('', () => {
-  it('', () => {
+import { srOnly as seOnlyMixin, srOnlyFocusable as srOnlyFocusableMixin } from '../Mixins/screenReader'
 
+import { srOnly, srOnlyFocusable } from './Screenreaders'
+
+describe('screen readers', () => {
+  it('sr-only', () => {
+    expect(srOnly).toEqual(seOnlyMixin)
+  })
+
+  it('sr-only-focusable', () => {
+    expect(srOnlyFocusable).toEqual(srOnlyFocusableMixin)
   })
 })
