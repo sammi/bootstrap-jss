04d8268db0047f4460c2d76651628cde3fd9714e
Yunhui <songyunhui2008@gmail.com>
2018-09-23 21:45:09 -0700

    Add shadow utility class.

---
+++ src/Utilities/Shadows.js
@@ -0,0 +1,24 @@
+import { boxShadowSm, boxShadow, boxShadowLg } from '../Variables/Components'
+
+const shadowSm = {
+  boxShadow: `${boxShadowSm} !important`
+}
+
+const shadow = {
+  boxShadow: `${boxShadow} !important`
+}
+
+const shadowLg = {
+  boxShadow: `${boxShadowLg} !important`
+}
+
+const shadowNone = {
+  boxShadow: 'none !important'
+}
+
+export {
+  shadowSm,
+  shadow,
+  shadowLg,
+  shadowNone
+}
--- src/Utilities/Shadows.test.js
+++ src/Utilities/Shadows.test.js
@@ -1,5 +1,30 @@
-describe('', () => {
-  it('', () => {
 
+import { boxShadowSm, boxShadow, boxShadowLg } from '../Variables/Components'
+
+import { shadowSm, shadow, shadowLg, shadowNone } from './Shadows'
+
+describe('shadow', () => {
+  it('shadow-sm', () => {
+    expect(shadowSm).toEqual({
+      boxShadow: `${boxShadowSm} !important` 
+    })
+  })
+
+  it('shadow', () => {
+    expect(shadow).toEqual({
+      boxShadow: `${boxShadow} !important`
+    })
+  })
+
+  it('shadow-lg', () => {
+    expect(shadowLg).toEqual({
+      boxShadow: `${boxShadowLg} !important` 
+    })
+  })
+
+  it('shadow-none', () => {
+    expect(shadowNone).toEqual({
+      boxShadow: 'none !important'
+    })
   })
 })
