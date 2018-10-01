2bac578be41fe9806a2de7b922e66858c02646f5
Yunhui <songyunhui2008@gmail.com>
2018-09-30 22:08:53 -0700

    fixed config file lint issue.

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,21 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
 
   const presets = [
-    "@babel/preset-env",
-    "@babel/preset-react"
-  ];
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ]
 
   const plugins = [
-    "@babel/plugin-transform-arrow-functions",
-    "@babel/plugin-transform-computed-properties",
-    "@babel/plugin-transform-spread",
-    "@babel/plugin-transform-template-literals",
-    "@babel/plugin-transform-for-of"
-  ];
+    '@babel/plugin-transform-arrow-functions',
+    '@babel/plugin-transform-computed-properties',
+    '@babel/plugin-transform-spread',
+    '@babel/plugin-transform-template-literals',
+    '@babel/plugin-transform-for-of'
+  ]
 
   return {
     presets,
     plugins
-  };
+  }
 }
