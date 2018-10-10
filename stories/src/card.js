8fdeeeddc2e691ec7ba6c9456da52f868be0d4f4
Yunhui <songyunhui2008@gmail.com>
2018-10-09 22:56:11 -0700

    Add 1 more card story.

--- stories/src/card.js
+++ stories/src/card.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { Reboot, Classes } from 'bootstrap-jss'
+import { Reboot, Classes, Utilities } from 'bootstrap-jss'
 import injectSheet from 'react-jss'
 
 const { btn, btnThemeColors } = Classes.Buttons
-const { card, cardImgTop, cardBody, cardTitle, cardText, cardGroup } = Classes.Card
+const { card, cardImgTop, cardBody, cardTitle, cardText, cardLink, cardSubtitle } = Classes.Card
+const { textMuted } = Utilities.Text
 
 const btnColors = btnThemeColors()
 
-const styles = {
+const Card = injectSheet({
   focus: {}, 
   disabled: {}, 
   active: {},
@@ -29,9 +30,7 @@ const styles = {
     ...card,
     width: '18rem'
   }
-}
-
-const Card = injectSheet(styles)(({classes}) => (
+})(({classes}) => (
   <div className={classes.card}>
     <img className={classes.cardImgTop} src="img/286_180.svg" />
     <div className={classes.cardBody}>
@@ -42,6 +41,47 @@ const Card = injectSheet(styles)(({classes}) => (
   </div>
 ))
 
+const CardContentType = injectSheet({
+  focus: {}, 
+  disabled: {}, 
+  active: {},
+  show: {},
+  listGroup: {},
+  listGroupItem: {},
+  dropdownToggle: {},
+  cardImgTop,
+  cardBody,
+  cardTitle,
+  cardText,
+  cardLink: {
+    ...cardLink,
+    ...Reboot.links.a,
+    marginRight: '0.25rem'
+  },
+  card: {
+    ...card,
+    width: '18rem'
+  },
+  cardSubtitle: {
+    ...cardSubtitle,
+    marginBottom: '0.25rem',
+    ...textMuted
+  }
+})(({classes}) => (
+  <div className={classes.card}>
+    <div className={classes.cardBody}>
+      <h5 className={classes.cardTitle}>Card title</h5>
+      <h6 className={classes.cardSubtitle}>Card subtitle</h6>
+      <p className={classes.cardText}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" className={classes.cardLink}>Card link</a>
+      <a href="#" className={classes.cardLink}>Another link</a>
+    </div>
+  </div>
+))
+
 storiesOf('Card', module)
   .add('card', () => <Card />)
+  .add('content type', () =>
+    <CardContentType/>
+  )
   
