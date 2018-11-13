21371899c7306352e7047f5632d3f576ee8d1458
Sammi Song <songyunhui2008@gmail.com>
2018-11-12 16:58:10 -0800

    Fix circle ci build error.

--- src/Mixins/breakpoints.js
+++ src/Mixins/breakpoints.js
@@ -2,8 +2,8 @@ import { size } from '../Functions/size'
 
 const breakpointNext = (name, breakpoints) => {
   let breakPointList = []
-  Object.keys(breakpoints).map(name => { breakPointList.push({ name, value: breakpoints[name] }) })
-  const sortBreakPointList = breakPointList.sort((a, b) => size(a.value).value > size(b.value).value)
+  Object.keys(breakpoints).forEach(name => { breakPointList.push({ name, value: breakpoints[name] }) })
+  const sortBreakPointList = breakPointList.sort((a, b) => size(a.value).value >= size(b.value).value)
 
   let namePosition = -1
   sortBreakPointList.forEach((b, i) => { namePosition = (b.name === name && i < sortBreakPointList.length) ? i + 1 : namePosition })
